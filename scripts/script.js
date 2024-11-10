18616b3e39d966e43c95e772524f03e0ac2ce938
Jasper Keith O. Vallecera <jasperkeith.vallecera@cit.edu>
2024-11-10 20:05:59 +0800

    Convert to class component

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,99 @@
-const pokemonContainer = document.querySelector(".pokemon-container");
-const searchInput = document.querySelector("input");
-const POKEMON_COUNT = 151; // First generation Pokemon
+class PokemonCard {
+  constructor(pokemon) {
+    this.pokemon = pokemon;
+    this.element = this.createElement();
+  }
 
-// Fetch all Pokemon data
-async function fetchPokemons() {
-  for (let i = 1; i <= POKEMON_COUNT; i++) {
-    await getPokemon(i);
+  createElement() {
+    const card = document.createElement("div");
+    card.classList.add("pokemon-card");
+
+    const name =
+      this.pokemon.name[0].toUpperCase() + this.pokemon.name.slice(1);
+    const id = this.pokemon.id.toString().padStart(3, "0");
+    const types = this.pokemon.types.map((type) => type.type.name);
+
+    card.innerHTML = `
+            <div class="img-container">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                  this.pokemon.id
+                }.png" alt="${name}">
+            </div>
+            <div class="info">
+                <span class="number">#${id}</span>
+                <h3 class="name">${name}</h3>
+                <small class="type">Type: <span>${types.join(
+                  ", "
+                )}</span></small>
+            </div>
+        `;
+
+    return card;
+  }
+
+  show() {
+    this.element.style.display = "block";
+  }
+
+  hide() {
+    this.element.style.display = "none";
   }
 }
 
-// Fetch individual Pokemon data
-async function getPokemon(id) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const response = await fetch(url);
-  const pokemon = await response.json();
-  createPokemonCard(pokemon);
-}
+class PokemonContainer {
+  constructor() {
+    this.container = document.querySelector(".pokemon-container");
+    this.searchInput = document.querySelector("input");
+    this.pokemonCards = [];
+    this.POKEMON_COUNT = 151;
 
-// Create Pokemon card
-function createPokemonCard(pokemon) {
-  const card = document.createElement("div");
-  card.classList.add("pokemon-card");
+    this.initialize();
+  }
 
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  const id = pokemon.id.toString().padStart(3, "0");
-  const types = pokemon.types.map((type) => type.type.name);
+  async initialize() {
+    this.setupSearchListener();
+    await this.fetchPokemons();
+  }
 
-  card.innerHTML = `
-        <div class="img-container">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              pokemon.id
-            }.png" alt="${name}">
-        </div>
-        <div class="info">
-            <span class="number">#${id}</span>
-            <h3 class="name">${name}</h3>
-            <small class="type">Type: <span>${types.join(", ")}</span></small>
-        </div>
-    `;
+  setupSearchListener() {
+    this.searchInput.addEventListener("input", (e) => {
+      const search = e.target.value.toLowerCase();
+      this.filterPokemon(search);
+    });
+  }
 
-  pokemonContainer.appendChild(card);
-}
-
-// Search functionality
-searchInput.addEventListener("input", function (e) {
-  const search = e.target.value.toLowerCase();
-  const pokemonCards = document.querySelectorAll(".pokemon-card");
-
-  pokemonCards.forEach((card) => {
-    const name = card.querySelector(".name").textContent.toLowerCase();
-    if (name.includes(search)) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
+  async fetchPokemons() {
+    for (let i = 1; i <= this.POKEMON_COUNT; i++) {
+      await this.getPokemon(i);
     }
-  });
-});
+  }
 
-// Initialize
-fetchPokemons();
+  async getPokemon(id) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const pokemon = await response.json();
+    this.createPokemonCard(pokemon);
+  }
+
+  createPokemonCard(pokemon) {
+    const card = new PokemonCard(pokemon);
+    this.pokemonCards.push(card);
+    this.container.appendChild(card.element);
+  }
+
+  filterPokemon(search) {
+    this.pokemonCards.forEach((card) => {
+      const name = card.element
+        .querySelector(".name")
+        .textContent.toLowerCase();
+      if (name.includes(search)) {
+        card.show();
+      } else {
+        card.hide();
+      }
+    });
+  }
+}
+
+// Initialize the app
+const pokedex = new PokemonContainer();
